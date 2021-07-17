651bf02c8ea2ba4de8bdb4debb1e1ca8b7c7e32b
dpk296 <84839099+dpk296@users.noreply.github.com>
2021-07-17 15:58:53 +0530

    Delete 6.py

--- 6.py
+++
@@ -1,7 +0,0 @@
-import array
-import binascii
-a = array.array('i', [1,2,3,4,5,6])
-print("Original array:")
-print('A1:', a)
-bytes_array = a.tobytes()
-print('Array of bytes:', binascii.hexlify(bytes_array))
